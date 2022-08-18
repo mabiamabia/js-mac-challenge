3bcfbbaec352ae92ee84a43ba903c2abdac5d87d
priscila.costa <priscila.costa@g.globo>
2022-08-18 15:37:16 -0300

    add calculate area challenge

---
+++ beecrowd/iniciante/1002.js
@@ -0,0 +1,9 @@
+
+ const pi = 3.14159;
+ const raio = 2;
+ const area = pi*(raio**2);
+
+console.log("A=" + area);
+
+//falta colocar um input na variavel de raio pro usuario fazer a entrada
+// bee não aceitou essa solução ainda
