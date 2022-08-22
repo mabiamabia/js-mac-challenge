f1802a718a886950537e43b23a89c814ec29ea51
priscila.costa <priscila.costa@g.globo>
2022-08-22 18:04:21 -0300

    Adiciona nova atividade

---
+++ beecrowd/iniciante/2234.js
@@ -0,0 +1,28 @@
+// H [e o numero de cachorro uquentes consumidos
+const prompt = require('prompt-sync');
+
+// P numero de pessoas que comeram esses cachorro quentes
+// divide H por P e mostra o resultado com duas casas depois da virgula
+
+
+// const HotDogJan = 0.11;
+// const HotDogFev = 76.36;
+// const HotDogMar = 0.02;
+// const HotDogAbr = 0.03;
+// const HotDogMai = 0.04;
+// const HotDogJun = 1.08;
+
+// const media = (HotDogJan+HotDogFev+HotDogMar+HotDogAbr+HotDogMai+HotDogJun) / 6;
+
+// console.log(media);
+
+const UnidadesJaneiro = prompt("Digite a quantidade de cachorros-quentes comidos em janeiro: ");
+const pessoasJaneiro = prompt("Digite a quantidade de pessoas que comeram em janeiro: ");
+const mediaJaneiro = UnidadesJaneiro / pessoasJaneiro
+
+
+
+const media = (mediaJaneiro)
+
+console.log(media);
+
